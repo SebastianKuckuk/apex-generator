0928aa83efc56694342f816a7cbac50b60a93531
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2024-11-12 10:20:15 +0100

    add initial plot script

---
+++ src/plot.py
@@ -0,0 +1,78 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import sys
+
+from apps import get_default_apps
+
+from backend.backend import Backend
+
+
+def plot(app, show_plot=False):
+    print(f'Running {app.group}/{app.name} ...')
+
+    # configure the folder containing the measurements
+    data_folder = Backend.default_measurement_dir(app)
+    data_folder.mkdir(parents=True, exist_ok=True)
+
+    # configure measurement data file and read it
+    data_file = data_folder / f'{app.name}.csv'
+    columns = ['index', 'gpu', 'backend', 'nx', 'ny', 'nz', 'nIt', 'nWarmUp', 'type', 'time', 'mlups', 'bandwidth', 'compute']
+    index = 'index'
+
+    df = pd.read_csv(data_file, header=0, names=columns, index_col=index)
+
+    # add auxiliary columns
+    df['numCells'] = df['nx'] * df['ny'] * df['nz']
+    df['version'] = df[['backend', 'gpu']].apply(lambda x: f'{x[0]} ({x[1]})', axis=1)
+
+    # filter by gpu
+    gpus = df['gpu'].unique()
+    df = df[df.apply(lambda x: x['gpu'] in gpus, axis=1)]
+
+    # filter by back end
+    backends = df['backend'].unique()
+    df = df[df.apply(lambda x: x['backend'] in backends, axis=1)]
+
+    # remove unnecessary columns and group by group_col
+    group_col = 'version'
+    df = df[[group_col, 'gpu', 'backend', 'numCells', 'bandwidth']]
+    df.set_index('numCells', inplace=True)
+    # df = df.groupby(group_col)
+
+    # plot resulting data frame
+    plot_file_name = data_file.with_suffix('.pdf')
+    print(f'Plotting results to \'{plot_file_name}\'')
+
+    df.groupby(group_col)['bandwidth'].plot(legend=True, logx=True, figsize=[11.7, 8.3])
+    plt.savefig(plot_file_name)
+
+    if show_plot:
+        plt.show()
+
+    # plot per back end
+    for backend in backends:
+        print(f'Plotting for back end {backend}')
+        plt.clf()
+        filtered = df[backend == df['backend']]
+        filtered.groupby('version')['bandwidth'].plot(legend=True, logx=True, figsize=[11.7, 8.3])
+        plt.savefig(data_folder / f'{app.name}-{backend.replace(" ", "")}.pdf')
+
+    # plot per gpu
+    for gpu in gpus:
+        print(f'Plotting for GPU {gpu}')
+        plt.clf()
+        filtered = df[gpu == df['gpu']]
+        filtered.groupby('version')['bandwidth'].plot(legend=True, logx=True, figsize=[11.7, 8.3])
+        plt.savefig(data_folder / f'{app.name}-{gpu.replace(" ", "")}.pdf')
+
+
+if len(sys.argv) < 2:
+    print(f'Usage: python {sys.argv[0]} app')
+    exit(1)
+
+cla_app = sys.argv[1]      # 'all'
+
+apps = get_default_apps()
+
+for app in apps[cla_app]:
+    plot(app)
