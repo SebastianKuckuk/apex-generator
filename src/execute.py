fa4102172a467a73704a53cd10608b25a7576735
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2024-11-12 10:15:19 +0100

    add execute script

---
+++ src/execute.py
@@ -0,0 +1,27 @@
+import sys
+
+from apps import get_default_apps
+
+from backends import get_default_backends
+
+
+if len(sys.argv) < 4:
+    print(f'Usage: python {sys.argv[0]} machine app backend')
+    exit(1)
+
+cla_machine = sys.argv[1]  # 'nvidia.alex.a40'
+cla_app = sys.argv[2]      # 'all'
+cla_backend = sys.argv[3]  # 'all'
+
+apps = get_default_apps()
+backends = get_default_backends(cla_machine)
+
+for app in apps[cla_app]:
+    print(f'Executing {app.group}/{app.name} ...')
+
+    for backend in backends[cla_backend]:
+        print(f'  ... for {backend.__name__}')
+
+        backend.exec_bin(app)
+
+    print()
