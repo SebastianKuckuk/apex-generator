4c98057ba1c8f6f38487c6cbaa19446a298cafaa
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2025-05-16 10:38:27 +0200

    add support for compound assignments

--- src/node/assignment.py
+++ src/node/assignment.py
@@ -1,7 +1,8 @@
 class Assignment:
-    def __init__(self, lhs, rhs):
+    def __init__(self, lhs, rhs, op='='):
         self.lhs = lhs
         self.rhs = rhs
+        self.op = op
 
     def __str__(self):
-        return f'{self.lhs} = {self.rhs};'
+        return f'{self.lhs} {self.op} {self.rhs};'
